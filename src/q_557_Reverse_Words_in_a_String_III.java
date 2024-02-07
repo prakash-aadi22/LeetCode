92b8f6eb093d8f76b361f2cfe4555762191017db
Aditya Prakash <prakashaditya2209@gmail.com>
2024-02-07 17:38:19 +0530

    07/02/2024

--- src/q_557_Reverse_Words_in_a_String_III.java
+++ src/q_557_Reverse_Words_in_a_String_III.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class q_557_Reverse_Words_in_a_String_III {
     public static String reverseWords(String s) {
-
+        String[] words = s.split("\\s+");
+        StringBuilder reversed = new StringBuilder();
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word);
+            reversedWord.reverse();
+            reversed.append(reversedWord).append(" ");
+        }
+        return reversed.toString().trim();
     }
 
     public static void main(String[] args) {
